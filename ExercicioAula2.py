bcb5047e4d7fee6b7f528f3a5f575bcc9ba1d72d
Juliana Sandes <84139776+JulianaSandes@users.noreply.github.com>
2024-03-13 17:50:05 -0300

    Add files via upload

---
+++ ExercicioAula2.py
@@ -0,0 +1,146 @@
+"""
+Exercicio 1
+
+1. Faça um programa que exiba seu nome na tela.
+"""
+
+print("Meu nome é Juliana Sandes")
+
+
+"""
+Exercicio 2
+
+2. Escreva um programa que exiba o resultado de 2a x
+3b, em que a vale 3 e b vale 5.
+"""
+a = 3
+b = 5
+print(2*a * 3*b)
+
+
+
+"""
+Exercicio 3
+
+3. Escreva um programa que calcule a soma de três
+variáveis e imprima o resultado na tela.
+"""
+
+a = 25
+b = 60
+c = 2000
+
+resultado = a + b + c
+print("O resultado é:", resultado)
+
+
+"""
+Exercicio 4
+
+4. Faça um programa que peça dois números inteiros.
+Imprima a soma desses dois números na tela.
+"""
+
+valorA= int(input("Informe um número:"))
+valorB = int(input("Informe um segundo número:"))
+
+
+resultado = valorA + valorB
+
+print("A soma é", resultado)
+
+
+"""
+Exercicio 5
+
+5. Escreva um programa que leia um valor em metros e
+o exiba convertido em milímetros.
+"""
+
+valorA= int(input("Informe um valor em metros, para transformar em milímetros:"))
+
+resultado = valorA * 1000
+
+print("O resultado é", resultado, "mm")
+
+
+"""
+Exercicio 6
+
+6. Escreva um programa que leia a quantidade de dias,
+horas, minutos e segundos do usuário. Calcule o total
+em segundos.
+"""
+
+"""
+Exercicio 7
+
+7. Faça um programa que calcule o aumento de um
+salário. Ele deve solicitar o valor do salário e a
+porcentagem do aumento. Exiba o valor do aumento e
+do novo salário. 
+"""
+
+"""
+Exercicio 8
+
+8. Faça um programa que solicite o preço de uma
+mercadoria e o percentual de desconto. Exiba o valor do
+desconto e o preço a pagar. 
+"""
+
+"""
+Exercicio 9
+
+9. Escreva um programa que calcule o tempo de uma
+viagem de carro. Pergunte a distância a percorrer e a
+velocidade média esperada para a viagem.
+"""
+
+"""
+Exercicio 10
+
+10. Escreva um programa que converta uma
+temperatura digitada em ºC para ºF. A fórmula para essa
+conversão é F = ((9 x C) / 5) + 32
+"""
+
+"""
+Exercicio 11
+
+11. Escreva um programa que pergunte a quantidade de
+km percorridos por um carro alugado pelo usuário, assim
+como a quantidade de dias pelos quais o carro foi
+alugado. Calcule o preço a pagar, sabendo que o carro
+custa R$60 por dia e R$0,15 por km rodado.
+"""
+
+"""
+Exercicio 12
+
+12. Escreva um programa que receba 2 valores do tipo
+inteiro x e y, e calcule o valor de z:
+z = (x2 + y2) / (x – y)2
+"""
+
+"""
+Exercicio 13
+
+13. Escreva um programa que receba o salário de um
+funcionário (float) e retorne o resultado do novo salário
+com reajuste de 35%.
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
